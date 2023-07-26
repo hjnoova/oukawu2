7a558effb71e798e6d8747cb70cb3d1c74268f1a
oukawu <oukawu@192.168.1.2>
2023-07-26 20:54:01 +0800

    test for droneCI

--- test.py
+++ test.py
@@ -1,1 +1,13 @@
-sdfwef
+from fastapi import FastAPI
+
+
+app = FastAPI()
+
+
+@app.get("/")
+def index():
+    return "this is index test page"
+
+@app.get("/root")
+def root():
+    return "this is root page for test only"
